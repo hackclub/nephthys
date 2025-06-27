29be9085ffe2c26c1db4099c4938539edea094d4
transcental <amber@transcental.dev>
2025-06-27 19:31:06 +0100

    add identity and faq macros

---
+++ nephthys/macros/faq.py
@@ -0,0 +1,29 @@
+from nephthys.actions.resolve import resolve
+from nephthys.data.transcript import Transcript
+from nephthys.macros.types import Macro
+from nephthys.utils.env import env
+
+
+class FAQ(Macro):
+    name = "faq"
+
+    async def run(self, ticket, helper, **kwargs):
+        """
+        A simple hello world macro that does nothing.
+        """
+        user_info = await env.slack_client.users_info(user=helper.slackId)
+        name = (
+            user_info["user"]["profile"].get("display_name")
+            or user_info["user"]["profile"].get("real_name")
+            or user_info["user"]["name"]
+        )
+        await env.slack_client.chat_postMessage(
+            text=f"hey, {name}! this question is answered in the faq i sent earlier, please make sure to check it out! :rac_cute:\n\n<here it is again|{Transcript.FAQ_LINK}>",
+            channel=env.slack_help_channel,
+            thread_ts=ticket.msgTs,
+        )
+        await resolve(
+            ts=ticket.msgTs,
+            resolver=helper.slackId,
+            client=env.slack_client,
+        )
