e3daf5571850b3a3f49aa90b8550006e31c4d93c
MMK21 <50421330+MMK21Hub@users.noreply.github.com>
2025-10-21 00:06:36 +0100

    Add a script for creating dummy DB records (#63)
    * Add a script for creating dummy DB records
    
    * Note that add_dummy_data takes a while to run
    
    * Change the dummy data range to 2 weeks
    
    * Ensure the dummy data script sets all the timestamps
    
    Previously it wouldn't set assignedAt, closedAt, etc, despite the ticket being marked as closed

---
+++ nephthys/scripts/add_dummy_data.py
@@ -0,0 +1,48 @@
+import asyncio
+import logging
+from datetime import datetime
+from datetime import timedelta
+from random import randint
+from sys import argv
+
+from nephthys.utils.env import env
+from nephthys.utils.logging import send_heartbeat
+from prisma.enums import TicketStatus
+
+
+async def create_dummy_records(num_records: int):
+    await env.db.connect()
+    # We generate random timestamps from up to 2 weeks ago
+    timestamp_a = datetime.now() - timedelta(weeks=2)
+    timestamp_b = datetime.now()
+    timestamp_range = (int(timestamp_a.timestamp()), int(timestamp_b.timestamp()))
+    for i in range(num_records):
+        timestamp = randint(*timestamp_range)
+        await env.db.ticket.create(
+            {
+                "title": f"Test ticket {i}",
+                "description": f"Test ticket number {i}",
+                "msgTs": f"{timestamp}.{i}",
+                # Let's say the bot message was sent 2 secs after
+                "ticketTs": f"{timestamp + 2}.{i}",
+                # User ID 1 should be the first user in the DB
+                "openedBy": {"connect": {"id": 1}},
+                # This prevents the "closing stale tickets" job getting angry at the fake tickets
+                "status": TicketStatus.CLOSED,
+                "closedBy": {"connect": {"id": 1}},
+                "assignedTo": {"connect": {"id": 1}},
+                # Let's say it took 10 secs to be assigned and 100 secs to be resolved
+                "assignedAt": datetime.fromtimestamp(timestamp + 10),
+                "closedAt": datetime.fromtimestamp(timestamp + 110),
+            }
+        )
+    await send_heartbeat(f"Created {num_records} dummy ticket records.")
+    logging.info(f"Successfully created {num_records} dummy ticket records.")
+
+
+if __name__ == "__main__":
+    if len(argv) < 2:
+        print("Usage: python add_dummy_data.py <num_records>")
+        exit(1)
+    num_records = int(argv[1])
+    asyncio.run(create_dummy_records(num_records))
