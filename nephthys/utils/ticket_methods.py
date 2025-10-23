01c17a71fbe545f1bfc98c1fc12c5e149deee501
RandomSearch <101704343+RandomSearch18@users.noreply.github.com>
2025-10-23 09:21:42 +0000

    Create delete_replies_to_ticket()

--- nephthys/utils/ticket_methods.py
+++ nephthys/utils/ticket_methods.py
@@ -1,6 +1,8 @@
 import logging
+from slack_sdk.errors import SlackApiError
 from slack_sdk.web.async_client import AsyncWebClient
 
+from nephthys.utils.delete_thread import add_message_to_delete_queue
 from nephthys.utils.env import env
 from prisma.models import Ticket
 
@@ -25,3 +27,10 @@ async def reply_to_ticket(ticket: Ticket, client: AsyncWebClient, text: str) -> 
             "ticket": {"connect": {"id": ticket.id}},
         }
     )
+
+
+async def delete_replies_to_ticket(ticket: Ticket, client: AsyncWebClient):
+    """Deletes all bot replies sent in a ticket thread"""
+    for bot_msg in ticket.userFacingMsgs or []:
+        await add_message_to_delete_queue(bot_msg.channelId, bot_msg.msgTs)
+        await env.db.botmessage.delete(where={"id": bot_msg.id})
