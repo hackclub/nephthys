e8671f91ec877e99bb4697acb9770250d59c344e
RandomSearch <101704343+RandomSearch18@users.noreply.github.com>
2025-10-23 08:56:38 +0000

    Fix type errors in reply_to_ticket()

--- nephthys/utils/ticket_methods.py
+++ nephthys/utils/ticket_methods.py
@@ -1,10 +1,11 @@
+import logging
 from slack_sdk.web.async_client import AsyncWebClient
 
 from nephthys.utils.env import env
 from prisma.models import Ticket
 
 
-def reply_to_ticket(ticket: Ticket, client: AsyncWebClient, text: str) -> None:
+async def reply_to_ticket(ticket: Ticket, client: AsyncWebClient, text: str) -> None:
     """Sends a user-facing message in the help thread and records it in the database"""
     channel = env.slack_help_channel
     thread_ts = ticket.msgTs
@@ -13,11 +14,14 @@ def reply_to_ticket(ticket: Ticket, client: AsyncWebClient, text: str) -> None:
         text=text,
         thread_ts=thread_ts,
     )
+    msg_ts = msg["ts"]
+    if not msg_ts:
+        logging.error(f"Bot message has no ts: {msg}")
+        return
     await env.db.botmessage.create(
         data={
-            "msgTs": msg["ts"],
+            "msgTs": msg_ts,
             "channelId": channel,
             "ticket": {"connect": {"id": ticket.id}},
         }
     )
-
