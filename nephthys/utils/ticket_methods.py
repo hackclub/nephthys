a69cffb9843d64c37fb07425f22a126fa2885f98
MMK21Hub <50421330+MMK21Hub@users.noreply.github.com>
2025-10-24 13:13:28 +0100

    Don't use delete queue in ticket_methods.py
    This is because the delete queue requires an admin token, ew

--- nephthys/utils/ticket_methods.py
+++ nephthys/utils/ticket_methods.py
@@ -1,10 +1,22 @@
 import logging
 
+from slack_sdk.errors import SlackApiError
 from slack_sdk.web.async_client import AsyncWebClient
 
-from nephthys.utils.delete_thread import add_message_to_delete_queue
 from nephthys.utils.env import env
 from prisma.models import Ticket
+
+
+async def delete_message(channel_id: str, message_ts: str):
+    """Deletes a Slack message, or does nothing if the message doesn't exist"""
+    try:
+        await env.slack_client.chat_delete(channel=channel_id, ts=message_ts)
+    except SlackApiError as e:
+        if e.response.get("error") != "message_not_found":
+            raise e
+        logging.warning(
+            f"Tried to delete message {message_ts} in channel {channel_id} but it is gone already."
+        )
 
 
 async def reply_to_ticket(ticket: Ticket, client: AsyncWebClient, text: str) -> None:
@@ -39,13 +51,13 @@ async def delete_bot_replies(ticket_ref: int):
     if not ticket.userFacingMsgs:
         raise ValueError(f"userFacingMsgs is not present on Ticket ID {ticket_ref}")
     for bot_msg in ticket.userFacingMsgs:
-        await add_message_to_delete_queue(bot_msg.channelId, bot_msg.ts)
+        await delete_message(bot_msg.channelId, bot_msg.ts)
         await env.db.botmessage.delete(where={"id": bot_msg.id})
 
 
 async def delete_and_clean_up_ticket(ticket: Ticket):
     """Removes a ticket from the DB and deletes all Slack messages associated with it"""
     await delete_bot_replies(ticket.id)
-    await add_message_to_delete_queue(env.slack_help_channel, ticket.ticketTs)
+    await delete_message(env.slack_help_channel, ticket.ticketTs)
     # TODO deal with DMs to tag subscribers?
     await env.db.ticket.delete(where={"id": ticket.id})
