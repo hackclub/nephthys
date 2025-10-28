90385e660b2b05e6f2618215255135852d5d52e7
MMK21Hub <50421330+MMK21Hub@users.noreply.github.com>
2025-10-28 22:24:49 +0000

    Fix header being wrapped in a tuple

--- nephthys/views/home/components/header.py
+++ nephthys/views/home/components/header.py
@@ -2,13 +2,11 @@ from nephthys.utils.env import env
 
 
 def get_header():
-    return (
-        {
-            "type": "header",
-            "text": {
-                "type": "plain_text",
-                "text": f":rac_cute: {env.app_title}",
-                "emoji": True,
-            },
+    return {
+        "type": "header",
+        "text": {
+            "type": "plain_text",
+            "text": f":rac_cute: {env.app_title}",
+            "emoji": True,
         },
-    )
+    }
