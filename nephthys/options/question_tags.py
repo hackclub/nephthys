7a7c83bbad0ea442701a0972c6b1ff2089025d1b
MMK21Hub <50421330+MMK21Hub@users.noreply.github.com>
2025-12-21 11:47:15 +0000

    Remove unused variable

--- nephthys/options/question_tags.py
+++ nephthys/options/question_tags.py
@@ -15,16 +15,16 @@ async def get_question_tags(payload: dict) -> list[dict[str, dict[str, str] | st
     if keyword:
         tag_names = [tag.label for tag in tags]
         scores = process.extract(keyword, tag_names, scorer=fuzz.ratio, limit=100)
-        old_tags = tags
-
-        tags = [old_tags[tag_names.index(score[0])] for score in scores]
+        matching_tags = [tags[tag_names.index(score[0])] for score in scores]
+    else:
+        matching_tags = tags
 
     res = [
         {
             "text": {"type": "plain_text", "text": f"{tag.label}"},
             "value": str(tag.id),
         }
-        for tag in tags
+        for tag in matching_tags
     ]
     logging.debug(res)
     return res
