8f5236881e609adae6301dde3483e31dc263c7e9
MMK21Hub <50421330+MMK21Hub@users.noreply.github.com>
2025-12-21 01:31:25 +0000

    Add a log!

--- nephthys/actions/assign_question_tag.py
+++ nephthys/actions/assign_question_tag.py
@@ -13,13 +13,16 @@ async def assign_question_tag_callback(
 ):
     await ack()
     user_id = body["user"]["id"]
-    selected = body["actions"][0]["value"]
-    if selected.lower() == "none":
+    selected = body["actions"][0]["selected_option"]
+    if not selected:
+        return
+    selected_value = selected["value"]
+    if selected_value.lower() == "none":
         return
     try:
-        tag_id = int(selected)
+        tag_id = int(selected_value)
     except ValueError as e:
-        raise ValueError(f"Invalid tag ID: {selected}") from e
+        raise ValueError(f"Invalid tag ID: {selected_value}") from e
 
     channel_id = body["channel"]["id"]
     ts = body["message"]["ts"]
@@ -45,3 +48,6 @@ async def assign_question_tag_callback(
             f"Failed to find corresponding ticket to update question tag ticket_ts={ts}"
         )
         return
+    logging.info(
+        f"Assigned question tag to ticket ticket_id={ticket.id} tag_id={tag_id}"
+    )
