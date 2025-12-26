5171cb9741eb797f893f327d071f7c49e9179069
Nullskulls <nullskulls@proton.me>
2025-12-26 18:22:02 +0200

    Added shipwrights macro

--- nephthys/macros/__init__.py
+++ nephthys/macros/__init__.py
@@ -10,6 +10,7 @@ from nephthys.macros.shipcertqueue import ShipCertQueue
 from nephthys.macros.thread import Thread
 from nephthys.macros.trigger_daily_stats import DailyStats
 from nephthys.macros.types import Macro
+from nephthys.macros.shipwrights import Shipwright
 from nephthys.utils.env import env
 from nephthys.utils.logging import send_heartbeat
 from prisma.enums import TicketStatus
@@ -27,6 +28,7 @@ macro_list: list[type[Macro]] = [
     Thread,
     Reopen,
     DailyStats,
+    Shipwright,
 ]
 
 macros = [macro() for macro in macro_list]
