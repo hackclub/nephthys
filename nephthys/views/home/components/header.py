478e687ef6fb2438d12479f55bbd84000bc899a6
MMK21Hub <50421330+MMK21Hub@users.noreply.github.com>
2026-01-18 13:32:40 +0000

    Add get_header_components()

--- nephthys/views/home/components/header.py
+++ nephthys/views/home/components/header.py
@@ -1,5 +1,7 @@
 from blockkit import Button
+from blockkit import Divider
 from blockkit import Header
+from blockkit.core import ModalBlock
 
 from nephthys.utils.env import env
 from prisma.models import User
@@ -40,16 +42,26 @@ def header_buttons(current_view: str, user: User | None):
         )
     )
 
-    return buttons.build()
+    return buttons
 
 
 def title_line():
-    return Header(f":rac_cute: {env.app_title}").build()
+    return Header(f":rac_cute: {env.app_title}")
 
 
 def get_header(user: User | None, current: str = "dashboard") -> list[dict]:
     return [
-        title_line(),
-        header_buttons(current, user),
+        title_line().build(),
+        header_buttons(current).build(),
         {"type": "divider"},
+    ]
+
+
+def get_header_components(
+    user: User | None, current: str = "dashboard"
+) -> list[ModalBlock]:
+    return [
+        title_line(),
+        header_buttons(current),
+        Divider(),
     ]
