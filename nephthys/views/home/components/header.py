37e6d378234f291d1e99ed233c61b5c12760d4da
MMK21Hub <50421330+MMK21Hub@users.noreply.github.com>
2026-01-18 13:17:08 +0000

    Use blockkit for header components

--- nephthys/views/home/components/header.py
+++ nephthys/views/home/components/header.py
@@ -1,3 +1,6 @@
+from blockkit import Button
+from blockkit import Header
+
 from nephthys.utils.env import env
 from prisma.models import User
 
@@ -5,55 +8,43 @@ from prisma.models import User
 def header_buttons(current_view: str, user: User | None):
     buttons = []
 
-    buttons.append(
-        {
-            "type": "button",
-            "text": {"type": "plain_text", "text": "Dashboard", "emoji": True},
-            "action_id": "dashboard",
-            **({"style": "primary"} if current_view != "dashboard" else {}),
-        }
+    buttons.add_element(
+        Button(
+            "Dashboard",
+            action_id="dashboard",
+            style="primary" if current_view != "dashboard" else None,
+        )
     )
 
-    buttons.append(
-        {
-            "type": "button",
-            "text": {"type": "plain_text", "text": "Assigned Tickets", "emoji": True},
-            "action_id": "assigned-tickets",
-            **({"style": "primary"} if current_view != "assigned-tickets" else {}),
-        }
+    buttons.add_element(
+        Button(
+            "Assigned Tickets",
+            action_id="assigned-tickets",
+            style="primary" if current_view != "assigned-tickets" else None,
+        )
     )
 
-    buttons.append(
-        {
-            "type": "button",
-            "text": {"type": "plain_text", "text": "Team Tags", "emoji": True},
-            "action_id": "team-tags",
-            **({"style": "primary"} if current_view != "team-tags" else {}),
-        }
+    buttons.add_element(
+        Button(
+            "Team Tags",
+            action_id="team-tags",
+            style="primary" if current_view != "team-tags" else None,
+        )
     )
 
-    buttons.append(
-        {
-            "type": "button",
-            "text": {"type": "plain_text", "text": "My Stats", "emoji": True},
-            "action_id": "my-stats",
-            **({"style": "primary"} if current_view != "my-stats" else {}),
-        }
+    buttons.add_element(
+        Button(
+            "My Stats",
+            action_id="my-stats",
+            style="primary" if current_view != "my-stats" else None,
+        )
     )
 
-    blocks = {"type": "actions", "elements": buttons}
-    return blocks
+    return buttons.build()
 
 
 def title_line():
-    return {
-        "type": "header",
-        "text": {
-            "type": "plain_text",
-            "text": f":rac_cute: {env.app_title}",
-            "emoji": True,
-        },
-    }
+    return Header(f":rac_cute: {env.app_title}").build()
 
 
 def get_header(user: User | None, current: str = "dashboard") -> list[dict]:
