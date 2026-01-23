3e14fbd0d3ab5d7f6e5e49ac48818c7df9d995c5
Mahad Kalam <55807755+skyfallwastaken@users.noreply.github.com>
2026-01-23 01:46:50 +0000

    yeah I really should go to bed

--- nephthys/events/message_creation.py
+++ nephthys/events/message_creation.py
@@ -321,7 +321,7 @@ async def generate_ticket_title(text: str):
                     "role": "system",
                     "content": (
                         "You are a helpful assistant that helps organise tickets for Hack Club's support team. You're going to take in a message and give it a title."
-                        "You will return no other content. You will return no other content. Do NOT use title case but use capital letter at start of sentence + use capital letters for terms/proper nouns."
+                        "You will return no other content. Do NOT use title case but use capital letter at start of sentence + use capital letters for terms/proper nouns."
                         "Avoid quote marks. Even if it's silly please summarise it. Use no more than 7 words, but as few as possible"
                         "When mentioning Flavortown, do *NOT* change it to 'flavor town' or 'flavour town'. Hack Club should *NOT* be changed to 'hackclub'."
                         "Hackatime, Flavortown, and Hack club should always be capitalized correctly. Same goes for terms like VSCode, PyCharm, API, and GitHub."
