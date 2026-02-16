a04a739a730824478b8ca02687679de7939d2136
MMK21Hub <50421330+MMK21Hub@users.noreply.github.com>
2026-02-16 21:45:23 +0000

    Remove Question Tags view button

--- nephthys/views/home/__init__.py
+++ nephthys/views/home/__init__.py
@@ -12,7 +12,6 @@ class View:
 APP_HOME_VIEWS: list[View] = [
     View("Dashboard", "dashboard"),
     View("Assigned Tickets", "assigned-tickets"),
-    View("Question Tags", "question-tags"),
     View("Team Tags", "team-tags"),
     View("My Stats", "my-stats"),
 ]
