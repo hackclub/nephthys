32247febfe6964be0112cf814ae22d3accce3ff4
Matthias Lubbertsen <Matthiaslubbertsen@gmail.com>
2026-04-16 21:32:18 +0200

    alias shipwhrights for typo i make often (#185)

--- nephthys/macros/shipwrights.py
+++ nephthys/macros/shipwrights.py
@@ -7,6 +7,7 @@ from nephthys.utils.ticket_methods import reply_to_ticket
 
 class Shipwrights(Macro):
     name = "shipwrights"
+    aliases = ["shipwhrights"]
 
     async def run(self, ticket, helper, **kwargs):
         """
