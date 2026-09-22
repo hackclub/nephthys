4f3ffc9ff96a5fd149478e6a5a25faa8f310721f
MMK21Hub <50421330+MMK21Hub@users.noreply.github.com>
2026-09-22 10:55:26 +0100

    Allow specifying a persistent sessions secret in .env
    And enforce that for production

--- nephthys/utils/env.py
+++ nephthys/utils/env.py
@@ -1,5 +1,6 @@
 import logging
 import os
+import secrets
 from dataclasses import dataclass
 from pathlib import Path
 from typing import Literal
@@ -8,6 +9,7 @@ from aiohttp import ClientSession
 from dotenv import load_dotenv
 from openai import AsyncOpenAI
 from slack_sdk.web.async_client import AsyncWebClient
+from starlette.datastructures import Secret
 
 from nephthys.transcripts import transcripts
 from nephthys.transcripts.transcript import Transcript
@@ -36,6 +38,35 @@ class HCAConfig:
     client_id: str
     client_secret: str
     base_url: str
+    session_secret: Secret
+
+
+def create_hca_config(environment: str) -> HCAConfig | None:
+    hca_client_id = os.environ.get("HCA_CLIENT_ID")
+    hca_client_secret = os.environ.get("HCA_CLIENT_SECRET")
+    session_secret = os.environ.get("SESSION_SECRET")
+    if (not hca_client_id) and (not hca_client_secret):
+        return None
+    if (not hca_client_id) or (not hca_client_secret):
+        raise ValueError(
+            "Both of HCA_CLIENT_ID and HCA_CLIENT_SECRET must be set; or neither must be set (for no HCA integration)"
+        )
+    if not session_secret:
+        if environment != "development":
+            raise ValueError(
+                "SESSION_SECRET environment variable must be set when HCA integration is enabled"
+            )
+        logging.info(
+            "Generating random session signing secret for development (set SESSION_SECRET to persist sessions)"
+        )
+        session_secret = secrets.token_urlsafe(32)
+
+    return HCAConfig(
+        client_id=hca_client_id,
+        client_secret=hca_client_secret,
+        base_url=os.environ.get("HCA_BASE_URL", "https://auth.hackclub.com"),
+        session_secret=Secret(session_secret),
+    )
 
 
 class Environment:
@@ -82,21 +113,7 @@ class Environment:
         self.daily_summary = get_environ_bool("DAILY_SUMMARY", default=True)
         self.enable_feedback = get_environ_bool("ENABLE_FEEDBACK", default=False)
         self.app_title = os.environ.get("APP_TITLE", "helper heidi")
-
-        hca_client_id = os.environ.get("HCA_CLIENT_ID")
-        hca_client_secret = os.environ.get("HCA_CLIENT_SECRET")
-        if hca_client_id and hca_client_secret:
-            self.hca = HCAConfig(
-                client_id=hca_client_id,
-                client_secret=hca_client_secret,
-                base_url=os.environ.get("HCA_BASE_URL", "https://auth.hackclub.com"),
-            )
-        elif (not hca_client_id) and (not hca_client_secret):
-            self.hca = None
-        else:
-            raise ValueError(
-                "Both of HCA_CLIENT_ID and HCA_CLIENT_SECRET must be set; or neither must be set (for no HCA integration)"
-            )
+        self.hca = create_hca_config(self.environment)
 
         self.port = int(os.environ.get("PORT", 3000))
 
--- nephthys/utils/starlette.py
+++ nephthys/utils/starlette.py
@@ -1,10 +1,7 @@
-import secrets
-
 from prometheus_client import CONTENT_TYPE_LATEST
 from prometheus_client import generate_latest
 from slack_bolt.adapter.starlette.async_handler import AsyncSlackRequestHandler
 from starlette.applications import Starlette
-from starlette.datastructures import Secret
 from starlette.middleware.sessions import SessionMiddleware
 from starlette.requests import Request
 from starlette.responses import JSONResponse
@@ -97,12 +94,13 @@ app = Starlette(
     lifespan=main,
 )
 
-# for HCA OAuth2
-app.add_middleware(
-    SessionMiddleware,
-    secret_key=Secret(secrets.token_urlsafe(32)),
-    max_age=365 * 86400,  # 365 days, in seconds
-)
+if env.hca:
+    # required for HCA OAuth2
+    app.add_middleware(
+        SessionMiddleware,
+        secret_key=env.hca.session_secret,
+        max_age=365 * 86400,  # 365 days, in seconds
+    )
 
 app.add_middleware(
     PrometheusMiddleware,
